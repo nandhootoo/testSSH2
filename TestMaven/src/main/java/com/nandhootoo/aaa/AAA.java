fd0f72eaae476932b63129b48c64ee07e5beb491
nandhootoo <ox.ccniit@qq.com>
2013-09-09 22:42:57 +0800

    TestMaven second

---
+++ TestMaven/src/main/java/com/nandhootoo/aaa/AAA.java
@@ -0,0 +1,10 @@
+package com.nandhootoo.aaa;
+
+public class AAA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestMaven/src/test/java/com/nandhootoo/bbb/BBB.java
@@ -0,0 +1,5 @@
+package com.nandhootoo.bbb;
+
+public class BBB {
+
+}
